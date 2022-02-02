23260f4d5fbcff437d8db01abcf41ecb98a5c603
Chanteng <yrnchanteng@gmail.com>
2022-02-02 12:23:35 +0000

    done with the login form

--- App.js
+++ App.js
@@ -1,20 +1,88 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput, TouchableOpacity } from 'react-native';
+import React from 'react';
 
-export default function App() {
+const App = () => {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.login}>Log in</Text>
+      <TextInput style={styles.input} placeholder='Username' />
+      <TextInput style={styles.input} placeholder='Password' />
+      <Text style={styles.forgot}>Forgot Password?</Text>
+      <TouchableOpacity style={styles.btn}>
+        <Text style={styles.btntxt}>Log In</Text>
+      </TouchableOpacity>
+      <View style={styles.alc}>
+        <Text style={styles.sign}>Don't have account?</Text>
+        <Text style={styles.signup}>Sign up</Text>
+      </View>
     </View>
   );
-}
+};
+
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    padding: 24,
+    borderWidth: 2,
+    borderColor: "blue",
+    margin: 50,
+    borderRadius: 30
   },
+  login: {
+    fontSize: 50,
+    color: "blue",
+    marginTop: 100,
+    fontWeight: "bold",
+  },
+
+input: {
+  borderBottomWidth: 1,
+  marginTop: 50,
+  fontSize: 16,
+  borderBottomColor: "blue"
+},
+
+forgot: {
+  textAlign: "right",
+  marginTop: 15,
+  fontSize: 16
+},
+
+btn: {
+  backgroundColor: "blue",
+  width: 200,
+  height: 50,
+   borderRadius: 10,
+  justifyContent: "center",
+  alignItems: "center",
+  alignSelf: "center",
+  marginTop: 70,
+},
+
+btntxt: {
+  fontSize: 24,
+  color: "white"
+},
+
+alc: {
+  flexDirection: "row",
+  marginTop: 50,
+  justifyContent: "center",
+  alignItems: "center",
+},
+
+sign: {
+  fontSize: 16
+},
+
+signup: {
+  fontSize: 16,
+  marginLeft: 5,
+  color: "blue"
+}
+
 });
+
+
+export default App;
